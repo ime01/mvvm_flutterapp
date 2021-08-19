4558c1594d72f9bbbd8408b6fed6dd44d7d1131a
AndroidPhrophet <ketuimevbore@yahoo.com>
2021-08-19 12:23:22 -0700

    Flutter Onboarding Screen implemented with introduction screen package

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mvvm_flutterapp/view/home.dart';
 import 'package:mvvm_flutterapp/view/notification.dart';
+import 'package:mvvm_flutterapp/view/onboarding_screen_view.dart';
 import 'package:mvvm_flutterapp/view/video_player_from_file.dart';
 import 'package:mvvm_flutterapp/view/video_player_from_network.dart';
 import 'package:mvvm_flutterapp/view/video_player_view.dart';
@@ -19,6 +20,7 @@ class MyApp extends StatelessWidget {
         '/videofromfile': (context) => VideoPLayerFromFile() ,
         '/videofromnetwork': (context) => VideoPLayerFromNetwork(),
         '/notification': (context) => MyNotification(),
+        '/videohome': (context) => VideoPLayerFromAssets(),
       },
 
       title: 'Flutter Demo',
@@ -26,7 +28,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
 
-      home: VideoPLayerFromAssets(),
+      home: OnBoardingScreen(),
     );
   }
 }
---
+++ lib/view/onboarding_screen_view.dart
@@ -0,0 +1,77 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+import 'package:mvvm_flutterapp/view/video_player_view.dart';
+
+class OnBoardingScreen extends StatelessWidget {
+  const OnBoardingScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => SafeArea(
+    child: IntroductionScreen(
+      pages: [
+        PageViewModel(
+          title: 'A reader lives a thosuand lives',
+          body: 'The man who never reads lives only one',
+          image: buildImage('assets/daggerjoke.jpg'),
+          decoration: getPageDecoration(),
+
+        ),
+        PageViewModel(
+          title: 'Android Kotlin is King',
+          body: 'Android kotlin Jet Pack Compose is the future',
+          image: buildImage('assets/jetpacklogo.png'),
+          decoration: getPageDecoration(),
+
+        ),
+        PageViewModel(
+          title: 'King of the jungle',
+          body: 'A lion is the fearless king of the jungle',
+          image: buildImage('assets/lion.jpg'),
+          decoration: getPageDecoration(),
+
+        ),
+        PageViewModel(
+          title: 'Tractor',
+          body: 'Useful Machine for farming',
+          image: buildImage('assets/tractor.png'),
+          decoration: getPageDecoration(),
+
+        ),
+        PageViewModel(
+          title: 'Drinks',
+          body: 'Get you some Cocktail Magaritas',
+          image: buildImage('assets/cocktailapplogo.png'),
+          decoration: getPageDecoration(),
+
+        ),
+      ],
+      showNextButton: false,
+
+      showSkipButton: false,
+
+      skip: Text("Skip", style: TextStyle(fontWeight: FontWeight.bold),),
+
+      onDone: () => gotoHome(context),
+      
+      done: Text('Got it', style: TextStyle(fontWeight: FontWeight.bold),),),
+  );
+
+  Widget buildImage(String path) =>
+      Center(child: Image.asset(path, width: 350));
+
+  PageDecoration getPageDecoration()=> PageDecoration(
+    titleTextStyle: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+    bodyTextStyle: TextStyle(fontSize: 20),
+    descriptionPadding: EdgeInsets.all(16).copyWith(bottom: 0),
+    imagePadding: EdgeInsets.all(24),
+    pageColor: Colors.orange
+
+  );
+
+  gotoHome(context) => Navigator.of(context).pushReplacement(
+    MaterialPageRoute(builder: (_)=> VideoPLayerFromAssets()),
+  );
+
+}
